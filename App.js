6593e04bf436ae1422b5d3c54072d5fabb4e26e2
panduramadhan2 <panduramadhan2@gmail.com>
2022-07-28 01:51:34 +0700

    Initial Entry

--- App.js
+++ App.js
@@ -1,15 +1,46 @@
-import { View, Text } from "react-native";
+import React, { useState } from 'react';
+import {
+  Text,
+  View,
+  StyleSheet,
+  SafeAreaView,
+  Platform,
+  StatusBar,
+} from 'react-native';
+import Constants from 'expo-constants';
+import { colors } from './src/utils/colors';
+import { Focus } from './src/features/Focus';
+import { Timer } from './src/features/Timer';
+import { FocusHistory } from './src/features/FocusHistory';
+
 
 export default function App() {
+  const [currentSubject, setCurrentSubject] = useState();
+  const [history, setHistory] = useState([]);
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Universal React with Expo</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      {!currentSubject ? (
+        <>
+          <Focus addSubject={setCurrentSubject} />
+          <FocusHistory history={history} />
+        </>
+      ) : (
+        <Timer 
+          focusSubject={currentSubject}
+          onTimerEnd={(subject) => {
+            setHistory([...history, subject])
+          }}
+          clearSubject={() => setCurrentSubject(null)}
+        />
+      )}
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+    backgroundColor: colors.darkBlue,
+  },
+});
